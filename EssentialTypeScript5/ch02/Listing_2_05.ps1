59c62a65faa5fc3604ef9e57673c5813307f2788
David Stevenson <drstevenson1958@yahoo.com>
2024-06-29 15:26:30 -0400

    Listing 2.5 Initializing the project folder
    cd todo
    npm init --yes

---
+++ EssentialTypeScript5/ch02/Listing_2_05.ps1
@@ -0,0 +1,19 @@
+# Listing 2.5 Initializing the project folder
+
+cd todo
+npm init --yes
+
+# Wrote to D:\drs\TypeScript\GitHub\TypeScript\EssentialTypeScript5\ch02\todo\package.json:
+
+# {
+#   "name": "todo",
+#   "version": "1.0.0",
+#   "description": "",
+#   "main": "index.js",
+#   "scripts": {
+#     "test": "echo \"Error: no test specified\" && exit 1"
+#   },
+#   "keywords": [],
+#   "author": "",
+#   "license": "ISC"
+# }
