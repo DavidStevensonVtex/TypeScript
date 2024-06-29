7142b9b8435809407fbbb9544034daafc1e5011c
David Stevenson <drstevenson1958@yahoo.com>
2024-06-29 15:14:48 -0400

    Listing 2.3: Install TypeScript
    npm install --global typescript@5.0.2

---
+++ EssentialTypeScript5/ch02/Listing_2_03.ps1
@@ -0,0 +1,8 @@
+npm install --global typescript@5.0.2
+
+# changed 1 package in 2s
+# npm notice 
+# npm notice New major version of npm available! 9.6.7 -> 10.8.1       
+# npm notice Changelog: https://github.com/npm/cli/releases/tag/v10.8.1
+# npm notice Run npm install -g npm@10.8.1 to update!
+# npm notice 
