b48e949ac52e172900e9b152454f2de4dd1c7099
David Stevenson <drstevenson1958@yahoo.com>
2024-06-29 21:27:23 -0400

    Listing 2.27 Compiling and executing

---
+++ EssentialTypeScript5/ch02/Listing_2_27.ps1
@@ -0,0 +1,11 @@
+# Listing 2.27 Compiling and executing
+
+tsc
+node dist/index.js
+
+# Output
+# Adam's Todo List (3 items to do)
+# 1       Buy Flowers
+# 2       Get Shoes
+# 3       Collect Tickets
+# 4       Call Joe        (complete)
