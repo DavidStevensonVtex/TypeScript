70c346a500c2e5e7039b35bb6f890593d6186d6f
David Stevenson <drstevenson1958@yahoo.com>
2024-06-30 11:16:46 -0400

    Listing 2.35 Installing type definitions

---
+++ EssentialTypeScript5/ch02/Listing_2_35.ps1
@@ -0,0 +1,10 @@
+# Listing 2.35 Installing type definitions
+
+npm install --save-dev @types/inquirer@9.0.3
+
+# added 4 packages, and audited 53 packages in 2s
+
+# 23 packages are looking for funding
+#   run `npm fund` for details
+
+# found 0 vulnerabilities
