e3fc6f1054fc3e8c33a174ddebe34d90b454a4db
David Stevenson <drstevenson1958@yahoo.com>
2024-06-30 11:38:07 -0400

    Listing 2.38 Filtering items in the index.ts file in the src folder
    Listing 2.39 Compiling and executing

---
+++ EssentialTypeScript5/ch02/Listing_2_39.ps1
@@ -0,0 +1,24 @@
+# Listing 2.39 Compiling and executing
+
+tsc
+node dist/index.js
+
+# Output
+# Adam's Todo List (3 items to do)
+# 1       Buy Flowers
+# 2       Get Shoes
+# 3       Collect Tickets
+# 4       Call Joe        (complete)
+# ? Choose option (Use arrow keys)
+# ❯ Show/Hide Completed 
+#   Quit 
+
+# Adam's Todo List (3 items to do)
+# 1       Buy Flowers
+# 2       Get Shoes
+# 3       Collect Tickets
+# ? Choose option       
+#   Show/Hide Completed 
+# ❯ Quit
+
+# Command.Quit  
