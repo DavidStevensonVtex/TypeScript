f414ae98f2013001b9ae331906769f99c47520ff
David Stevenson <drstevenson1958@yahoo.com>
2024-06-30 15:10:30 -0400

    Listing 3.4 Starting the JavaScript file monitor

---
+++ EssentialTypeScript5/ch03/Listing_3-04.ps1
@@ -0,0 +1,11 @@
+# Listing 3.4 Starting the JavaScript file monitor
+
+npx nodemon index.JavaScript
+
+# [nodemon] 2.0.20
+# [nodemon] to restart at any time, enter `rs`       
+# [nodemon] watching path(s): *.*
+# [nodemon] watching extensions: js,mjs,json
+# [nodemon] starting `node index.JavaScript index.js`
+# Hat price: 100
+# [nodemon] clean exit - waiting for changes before restart
