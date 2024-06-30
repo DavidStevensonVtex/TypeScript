a9726f9f89f4ee197f12f69ed9ad6eeee0cb222e
David Stevenson <drstevenson1958@yahoo.com>
2024-06-29 21:42:41 -0400

    Listing 2.32 Adding a package to the project

---
+++ EssentialTypeScript5/ch02/Listing_2_32.ps1
@@ -0,0 +1,12 @@
+# Listing 2.32 Adding a package to the project
+
+npm install inquirer@9.1.4
+
+# Output
+
+# added 48 packages, and audited 49 packages in 9s
+
+# 23 packages are looking for funding
+#   run `npm fund` for details       
+
+# found 0 vulnerabilities
