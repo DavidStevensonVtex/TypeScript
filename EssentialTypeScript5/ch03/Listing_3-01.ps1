f65b1de7008a46f44f47928d1e704f57a1d4e663
David Stevenson <drstevenson1958@yahoo.com>
2024-06-30 15:04:56 -0400

    Listing 3.1 - Preparing the project folder

---
+++ EssentialTypeScript5/ch03/Listing_3-01.ps1
@@ -0,0 +1,20 @@
+# Listing 3.1 - Preparing the project folder
+
+cd primer 
+npm init --yes
+
+# PS D:\drs\TypeScript\GitHub\TypeScript\EssentialTypeScript5\ch03\primer> npm init --yes
+# Wrote to D:\drs\TypeScript\GitHub\TypeScript\EssentialTypeScript5\ch03\primer\package.json:
+
+# {
+#   "name": "primer",
+#   "version": "1.0.0",
+#   "description": "",
+#   "main": "index.js",
+#   "scripts": {
+#     "test": "echo \"Error: no test specified\" && exit 1"
+#   },
+#   "keywords": [],
+#   "author": "",
+#   "license": "ISC"
+# }
