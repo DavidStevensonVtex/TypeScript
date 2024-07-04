3d0a9511a2cd07917f3f502db92284b16805e0bc
David Stevenson <drstevenson1958@yahoo.com>
2024-07-04 19:52:24 -0400

    Listing 5.10 Running the compiler

---
+++ EssentialTypeScript5/ch05/Listing_5-10.ps1
@@ -0,0 +1,7 @@
+# Listing 5.10 Running the compiler
+
+tsc
+
+# src/index.ts:7:14 - error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+# 7 printMessage(100);
