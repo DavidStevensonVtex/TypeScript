72217babc6a85c92b7fdde1b0e31bf26ee5e3739
David Stevenson <drstevenson1958@yahoo.com>
2024-07-04 18:25:15 -0400

    Listing 5.7a Displaying the local list of files for compilation

---
+++ EssentialTypeScript5/ch05/Listing_5-07-a.ps1
@@ -0,0 +1,68 @@
+# Listing 5.7a Displaying the local list of files for compilation
+
+# If you need to run the compiler from the packages installed locally 
+# in the project, then you can use the npx command:
+
+npx tsc --listFiles
+
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es5.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2016.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2021.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.d.ts      
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.dom.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.dom.iterable.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.webworker.importscripts.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.scripthost.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.core.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.collection.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.generator.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.iterable.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.promise.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.proxy.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.reflect.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.symbol.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2015.symbol.wellknown.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2016.array.include.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.object.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.sharedmemory.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.string.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2017.typedarrays.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.asyncgenerator.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.asynciterable.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.promise.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2018.regexp.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.array.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.object.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.string.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.symbol.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2019.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.bigint.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.date.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.promise.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.sharedmemory.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.string.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.symbol.wellknown.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2020.number.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2021.promise.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2021.string.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2021.weakref.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2021.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.array.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.error.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.intl.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.object.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.sharedmemory.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.string.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.regexp.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.decorators.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.decorators.legacy.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/node_modules/typescript/lib/lib.es2022.full.d.ts
+# D:/drs/TypeScript/GitHub/TypeScript/EssentialTypeScript5/ch05/tools/src/index.ts
