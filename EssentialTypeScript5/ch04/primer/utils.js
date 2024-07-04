0844582df44b972bbf4aa7b54232bbde3a9cca47
David Stevenson <drstevenson1958@yahoo.com>
2024-07-04 12:05:16 -0400

    Listing 4.32 The contents of the utils.js file inthe primer folder

---
+++ EssentialTypeScript5/ch04/primer/utils.js
@@ -0,0 +1,12 @@
+// Listing 4.32 The contents of the utils.js file in the primer folder
+
+import { calculateTax } from "./tax";
+
+export function printDetails(product) {
+    let taxedPrice = calculateTax(product.price);
+    console.log(`Name: ${product.name}, Taxed Price: ${taxedPrice}`);
+}
+
+export function applyDiscount(product, discount = 5) {
+    product.price = product.price - discount ;
+}
