25a5e0068da80ba9979c2e03eaf26a1adcd735a4
David Stevenson <drstevenson1958@yahoo.com>
2024-07-05 19:06:42 -0400

    Listing 6.3 Adding the debugger keyword in the index.ts file in the src folder

--- EssentialTypeScript5/ch06/tools/src/index.ts
+++ EssentialTypeScript5/ch06/tools/src/index.ts
@@ -1,12 +1,13 @@
-// Listing 5.27 Adding a file extension in the index.ts file in the src folder
+// Listing 6.3 Adding the debugger keyword in the index.ts file in the src folder
 
 import { sum } from "./calc.js";
 
-let printMessage = (msg: string): void =>
-    console.log(`Message: ${msg}`);
+let printMessage = (msg: string): void => console.log(`Message: ${msg}`);
 
 let message = ("Hello, TypeScript");
 printMessage(message);
+
+debugger;
 
 let total = sum(100, 200, 300);
 console.log(`Total: ${total}`);
