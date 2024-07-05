d52cf2e35377e415882e50dfa0ee5ef272e0c0fa
David Stevenson <drstevenson1958@yahoo.com>
2024-07-05 19:31:15 -0400

    Listing 6.9 Adding eslint packages to the example project

---
+++ EssentialTypeScript5/ch06/Listing_6-09.ps1
@@ -0,0 +1,5 @@
+# Listing 6.9 Adding eslint packages to the example project
+
+npm install --save-dev eslint@8.36.0
+npm install --save-dev @typescript-eslint/parser@5.55.0
+npm install --save-dev @typescript-eslint/eslint-plugin@5.55.0
