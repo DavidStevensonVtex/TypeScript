e8b486626dd971429e9b248d1fd4a2622caffd96
David Stevenson <drstevenson1958@yahoo.com>
2024-07-04 20:07:10 -0400

    Listing 5.15 Starting the package command: npx tsc-watch --onsuccess "node dist/index.js"

---
+++ EssentialTypeScript5/ch05/Listing_5-15.ps1
@@ -0,0 +1,9 @@
+# Listing 5.15 Starting the package command: npx tsc-watch --onsuccess "node dist/index.js"
+
+npx tsc-watch --onsuccess "node dist/index.js"
+
+# Output
+# 8:05:21 PM - File change detected. Starting incremental compilation...
+
+# 8:05:23 PM - Found 0 errors. Watching for file changes.
+# Message: Hello, TypeScript
