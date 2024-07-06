3a487366ad7ee252cbfb26c472cf8bf0af0b15fe
David Stevenson <drstevenson1958@yahoo.com>
2024-07-06 10:02:28 -0400

    Listing 7.6 Starting the TypeScript compiler
    npm start

---
+++ EssentialTypeScript5/ch07/Listing_7-06.ps1
@@ -0,0 +1,8 @@
+# Listing 7.6 Starting the TypeScript compiler
+
+npm start
+
+# 10:01:44 AM - Starting compilation in watch mode...
+
+# 10:01:45 AM - Found 0 errors. Watching for file changes.
+# Hello, TypeScript
