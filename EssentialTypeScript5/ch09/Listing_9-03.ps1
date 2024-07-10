6672f21e6968f2e0ab5c7fb361fdc7ac5730d197
David Stevenson <drstevenson1958@yahoo.com>
2024-07-10 19:40:38 -0400

    Listing 9.3 Starting the TypeScript compiler

---
+++ EssentialTypeScript5/ch09/Listing_9-03.ps1
@@ -0,0 +1,8 @@
+# Listing 9.3 Starting the TypeScript compiler
+# package.json: "start": "tsc-watch --onsuccess \"node dist/index.js\""
+
+npm start
+
+# Price for Hat: $120.00
+# Price for Gloves: $90.00  
+# Price for Umbrella: $50.40
