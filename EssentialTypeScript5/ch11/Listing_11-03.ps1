3d486170f3bbf4e1f053622050458d5c64e19240
David Stevenson <drstevenson1958@yahoo.com>
2024-07-14 12:15:38 -0400

    Listing 11.3 Starting the TypeScript compiler

---
+++ EssentialTypeScript5/ch11/Listing_11-03.ps1
@@ -0,0 +1,8 @@
+# Listing 11.3 Starting the TypeScript compiler
+
+npm start
+
+# 12:15:09 PM - Found 0 errors. Watching for file changes.
+# bsmith Bob Smith, London
+# ajones Alice Jones, Paris    
+# dpeters Dora Peters, New York
