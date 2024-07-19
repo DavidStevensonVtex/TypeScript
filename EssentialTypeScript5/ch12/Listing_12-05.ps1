0906c39d069de37fb9951f8c08287eacfa5d9e86
David Stevenson <drstevenson1958@yahoo.com>
2024-07-19 19:55:38 -0400

    Listing 12.5 Starting the TypeScript compiler

---
+++ EssentialTypeScript5/ch12/Listing_12-05.ps1
@@ -0,0 +1,9 @@
+# Listing 12.5 Starting the TypeScript compiler
+
+npm start
+
+# 7:55:17 PM - Found 0 errors. Watching for file changes.
+# Item: Bob Smith
+# Item: Dora Peters  
+# Item: Running Shoes
+# Item: Hat
