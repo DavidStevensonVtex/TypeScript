7197f4cca925dc4f038e5c22571c5d49115ea533
David Stevenson <drstevenson1958@yahoo.com>
2024-07-21 20:58:32 -0400

    Listing 14.25 Using an accessor decorator in the product.ts file in the src folder
    Listing 14.24 The contents of the accessorDecorator.ts file in the src folder

--- EssentialTypeScript5/ch14/decorators/src/accessorDecorator.ts
+++ EssentialTypeScript5/ch14/decorators/src/accessorDecorator.ts
@@ -1,6 +1,6 @@
 // Listing 14.24 The contents of the accessorDecorator.ts file in the src folder
 
-export function log(accessor: any, ctx: ClassAccessorDecoratorContext | ClassGetterDecoratorContext) {
+export function log(accessor: any, ctx: ClassSetterDecoratorContext | ClassGetterDecoratorContext) {
     const name = String(ctx.name);
     return function (this: any, ...args: any[]) {
         if (ctx.kind === "getter") {
