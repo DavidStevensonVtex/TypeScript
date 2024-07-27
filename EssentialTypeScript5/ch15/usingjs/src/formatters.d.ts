9ab6f6e84f792f6625ee3073d2bee05ab2759e3f
David Stevenson <drstevenson1958@yahoo.com>
2024-07-27 18:11:08 -0400

    Listing 15.18 The contents of the formatter.d.ts file in the src folder

---
+++ EssentialTypeScript5/ch15/usingjs/src/formatters.d.ts
@@ -0,0 +1,8 @@
+// Listing 15.18 The contents of the formatter.d.ts file in the src folder
+
+export declare function sizeFormatter(thing: string, count: number): void;
+export declare function costFormatter(thing: string, cost: number | string): void;
+
+// Output
+// The Cart has 4 items
+// The Cart costs $341.3
