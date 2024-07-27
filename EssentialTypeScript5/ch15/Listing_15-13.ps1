f3aaba1844f50f88a3a6aac84cfdb99ba20058ed
David Stevenson <drstevenson1958@yahoo.com>
2024-07-27 17:44:49 -0400

    Listing 15.13 Starting the compiler

---
+++ EssentialTypeScript5/ch15/Listing_15-13.ps1
@@ -0,0 +1,7 @@
+# Listing 15.13 Starting the compiler
+
+npm start
+
+# src/formatters.js(8,60): error TS2554: Expected 0-1 arguments, but got 2.
+
+# 5:44:21 PM - Found 1 error. Watching for file changes.
