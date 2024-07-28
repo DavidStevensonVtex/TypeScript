909e275437e5bd5c59b783d68de94be7ef7de649
David Stevenson <drstevenson1958@yahoo.com>
2024-07-28 14:33:39 -0400

    Listing 15.33 Running the compiler

---
+++ EssentialTypeScript5/ch15/Listing_15-33.ps1
@@ -0,0 +1,10 @@
+# Listing 15.33 Running the compiler
+
+tsc --traceResolution
+
+# Output
+# ParserError: D:\drs\TypeScript\GitHub\TypeScript\EssentialTypeScript5\ch15\Listing_15-33.ps1:10:4
+# Line |
+#   10 |  26 console.log(chalk.notAColor("Formatted message"));
+#      |     ~~~~~~~~~~~
+#      | Unexpected token 'console.log' in expression or statement.
