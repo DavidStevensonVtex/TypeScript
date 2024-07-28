5aa98b0a87be74258c6a6dc3b52488f1b943dd8d
David Stevenson <drstevenson1958@yahoo.com>
2024-07-28 15:29:02 -0400

    Listing 16.6 Adding packages for webpack to the example project

---
+++ EssentialTypeScript5/ch16/Listing_16-06.ps1
@@ -0,0 +1,5 @@
+# Listing 16.6 Adding packages for webpack to the example project
+
+npm install --save-dev webpack@5.76.3
+npm install --save-dev webpack-cli@5.0.1
+npm install --save-dev ts-loader@9.4.2
