56c57346c90ab48bb05a853dc356931ea83914bf
David Stevenson <drstevenson1958@yahoo.com>
2024-07-28 15:34:36 -0400

    Listing 16.7 The contents of the webpack.config.js file in the webapp folder

---
+++ EssentialTypeScript5/ch16/webapp/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.ts",
+    output: { filename: "bundle.js" },
+    resolve: { extensions: [ ".ts", ".js" ] },
+    module: {
+        rules: {
+            rules: [
+                { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }
+            ]
+        }
+    }
+}
