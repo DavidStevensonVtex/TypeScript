baff28923b1fb7797fe11ad1e6dd12b508bc7bae
David Stevenson <drstevenson1958@yahoo.com>
2024-07-28 14:21:39 -0400

    Listing 15.29 Running the compiler

---
+++ EssentialTypeScript5/ch15/Listing_15-29.ps1
@@ -0,0 +1,8 @@
+# Listing 15.29 Running the compiler 
+
+tsc --traceResolution
+
+# Output
+# src/index.ts:20:31 - error TS2554: Expected 1 arguments, but got 2.
+
+# 20 let db = debug("Example App", true);
