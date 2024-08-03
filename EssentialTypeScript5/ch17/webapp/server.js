250ee1e6634b6ce5a90e25e06f7a2fc087d76eac
David Stevenson <drstevenson1958@yahoo.com>
2024-08-03 10:22:04 -0400

    Listing 17.16 The contents of the server.js file in the webapp folder

---
+++ EssentialTypeScript5/ch17/webapp/server.js
@@ -0,0 +1,15 @@
+// Listing 17.16 The contents of the server.js file in the webapp folder
+
+const express = require("express");
+const jsonSrever = require("json-server");
+
+const app = express();
+app.use("/", express.static("dist"));
+app.use("/", express.static("assets"));
+
+const router = jsonServer.router("data.json");
+app.use(jsonServer.bodyParser);
+app.use("/api", (req, resp, next) => router(req, resp, next));
+
+const port = process.argv[3] || 4000 ;
+app.listen(port, () => console.log(`Running on port ${port}`));
