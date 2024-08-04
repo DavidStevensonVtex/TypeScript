1fbdc4e69e14244a074b1dbe8557d3885692170a
David Stevenson <drstevenson1958@yahoo.com>
2024-08-04 14:00:05 -0400

    Listing 20.18 The contents of the selectionSlice.ts file in the src/data folder.

---
+++ EssentialTypeScript5/ch20/reactapp/src/data/selectionSlice.ts
@@ -0,0 +1,19 @@
+// Listing 20.18 The contents of the selectionSlice.ts file in the src/data folder.
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product, ProductSelection, ProductSelectionMutations } from "./entities";
+
+const productSelectionSlice = createSlice({
+    name: "selections",
+    initialState: Array<ProductSelection>(),
+    reducers: {
+        addToOrder(selections: ProductSelection[],
+            action: PayloadAction<[Product, number]>) {
+            ProductSelectionMutations.addProduct(selections,
+                action.payload[0], action.payload[1]);
+        }
+    }
+});
+
+export const reducer = productSelectionSlice.reducer;
+export const { addToOrder } = productSelectionSlice.actions;
