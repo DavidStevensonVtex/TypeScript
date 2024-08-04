c26f9d7393d271a56ddf4d3368d6ab71b5e49328
David Stevenson <drstevenson1958@yahoo.com>
2024-08-04 13:43:08 -0400

    Listing 20.17 Adding packages to the example project

---
+++ EssentialTypeScript5/ch20/Listing_20-17.ps1
@@ -0,0 +1,6 @@
+# Listing 20.17 Adding packages to the example project
+
+npm install redux@4.2.1
+npm install react-redux@8.0.5
+npm install @reduxjs/toolkit@1.9.3
+npm install --save-dev @types/react-redux@7.1.25
