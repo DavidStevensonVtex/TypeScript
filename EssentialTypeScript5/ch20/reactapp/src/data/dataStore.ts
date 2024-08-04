2c1fba032ce9b18126a4b8ccbd034daabeb05172
David Stevenson <drstevenson1958@yahoo.com>
2024-08-04 14:14:28 -0400

    Listing 20.19 The contents of the dataStore.ts file in the src/data folder

---
+++ EssentialTypeScript5/ch20/reactapp/src/data/dataStore.ts
@@ -0,0 +1,22 @@
+// Listing 20.19 The contents of the dataStore.ts file in the src/data folder
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import { reducer as selectionsReducer, addToOrder } from "./selectionSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const dataStore = configureStore({
+    reducer: {
+        "selections": selectionsReducer
+    }
+});
+
+export type AppDispatch = typeof dataStore.dispatch;
+export type RootState = ReturnType<typeof dataStore.getState>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const reducers = {
+    addToOrder
+}
