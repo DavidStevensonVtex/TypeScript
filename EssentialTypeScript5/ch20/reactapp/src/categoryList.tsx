90baaa1eeda28a4a7691eb7558dc3756a4d871e9
David Stevenson <drstevenson1958@yahoo.com>
2024-08-04 13:23:43 -0400

    Fix error: Type 'Set<string>' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.
    Change tsconfig.json: Add "ES2022" (minimum (ES2015) to "lib" section.
    
        "lib": [
          "ES2022",
          "dom",
          "dom.iterable",
          "esnext"
        ],

--- EssentialTypeScript5/ch20/reactapp/src/categoryList.tsx
+++ EssentialTypeScript5/ch20/reactapp/src/categoryList.tsx
@@ -9,12 +9,15 @@ interface Props {
 }
 
 export const CategoryList: FunctionComponent<Props> = (props => {
+    // console.log("CatoryList: props.categories: ", props.categories, "props.selected: ", props.selected);
     return <div className="d-grid gap-2">
         {["All", ...props.categories].map(c => {
             let btnClass = props.selected === c ? "btn-primary" : "btn-secondary";
             return <button key={c}
                 className={`btn btn-block ${btnClass}`}
-                onClick={() => props.selectCategory(c)} />
+                onClick={() => props.selectCategory(c)}>
+                {c}
+            </button>
         })}
     </div>
 })
